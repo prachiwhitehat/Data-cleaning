6a1f777ef1515b3aa6303de37ce4fd2049bc63be
prachiwhitehat <71639231+prachiwhitehat@users.noreply.github.com>
2022-11-20 00:12:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -70,16 +70,3 @@ del df["st_radlim"]
 del df["rowupdate"]
 del df["pl_facility"]
 
-df = df.rename({
-                'pl_hostname': "solar_system_name", 
-                'pl_discmethod': "planet_discovery_method", 
-                'pl_orbincl': "planet_orbital_inclination", 
-                'pl_dens': "planet_density", 
-                'ra_str': "right_ascension", 
-                'dec_str': "declination", 
-                "st_teff": "host_temperature", 
-                'st_mass': "host_mass", 
-                'st_rad': "host_radius"
-            }, axis='columns')
-
-df.to_csv('main.csv') 
